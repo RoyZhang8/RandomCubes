63b0f1a99f59ee912073364bae401740c44aad05
RoyZhang8 <royzhang991220@gmail.com>
2022-01-24 15:10:21 -0600

    Updated unity
    Create cube

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,29 @@
+/****
+ * Created By: Ruoyu Zhang
+ * Data Created: Jan 24, 2022
+ * 
+ * Last Edited by: NA
+ * Last Edited: Jan 24, 2022
+ * 
+ * Descripetion: Spawn multiple cubes prefabs into the secen.
+ * ***/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
